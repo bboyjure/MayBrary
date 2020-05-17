5bdc81ede36ea2dbcb1845fe50244707b3adb608
Jure Beton <jure.beton@gmail.com>
2020-05-17 13:57:45 +0200

    third save

--- routes/books.js
+++ routes/books.js
@@ -6,12 +6,12 @@ const multer = require("multer")
 const path = require("path")
 const imageMimeType = ["image/jpeg", "image/png", "image/gif"]
 const uploadPath = path.join("public", Book.coverImageBasePath)
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) =>{
-        callback(null, )
-    }
-})
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) =>{
+//         callback(null, )
+//     }
+// })
 
 //All books route
 router.get("/", async (req,res) =>{
